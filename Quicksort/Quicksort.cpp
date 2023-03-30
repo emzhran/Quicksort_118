5572a5037409c128d7e29fe957d1e8785471887f
emzhran <zachran750@gmail.com>
2023-03-30 15:52:46 +0700

    tambahan void display

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -84,6 +84,20 @@ void q_sort(int low, int high)
     q_sort(j + 1, high);
 
 }
+
+void display() {
+    cout << "\n----------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements" << mov_count << endl;
+}
     int main();
 
 
